2b32696bf97dafcc64abd5224b1a4caaed590762
dinia28 <syamsyiyatuddiniyah@gmail.com>
2024-11-08 18:36:56 +0700

    Update apk.py

--- apk.py
+++ apk.py
@@ -300,10 +300,12 @@ with st.container():
                 for weights in weights_options:
                     for metric in metric_options:
                         start_time = time.time()
-                        
-                        # Buat model dengan kombinasi parameter yang berbeda
-                        knn_model = KNeighborsClassifier(n_neighbors=n_neighbors, weights=weights, metric=metric)
-                        knn_model.fit(X_train, y_train)
+                # Menentukan nilai p untuk metrik Minkowski
+                p = 2 if metric == 'euclidean' else 1
+                
+                # Membuat model dengan parameter n_neighbors, weights, dan p
+                knn_model = KNeighborsClassifier(n_neighbors=n_neighbors, weights=weights, p=p)
+                knn_model.fit(X_train, y_train)
                         
                         # Evaluasi model
                         accuracy = knn_model.score(X_test, y_test)
